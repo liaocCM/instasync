cb69dd843cf89ca2131e90fb070a6b9a9234591c
Tex Liao <linex1008@gmail.com>
2024-09-05 11:26:09 +0800

    feat: add chevron down animation to tailwind config

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -66,12 +66,18 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "chevron-down": {
+          from: { transform: "translateY(-5%)" },
+          "50%": { transform: "translateY(5%)" },
+          to: { transform: "translateY(-5%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "chevron-down": "chevron-down 1s ease-in-out infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
